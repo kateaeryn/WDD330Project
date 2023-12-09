df7e459aaa466f1cb88828ef272f7005d2af86af
kateaeryn <kate.ae.christensen@gmail.com>
2023-12-09 09:48:31 -0700

    added placeholder image and fixed watchlist

--- src/js/account.js
+++ src/js/account.js
@@ -36,14 +36,15 @@ function renderListItem(item) {
 }
 function renderList() {
   let list = localStorage.getItem("wishlist");
-  let newList = JSON.parse(list);
-  if (newList.length === 0) {
+  list = list ? localStorage.getItem("wishlist") : [];
+  if (list == "[]") {
     const watchlist = document.querySelector(".watchList");
     const message = document.createElement("h1");
     message.innerText =
       "There's nothing here . . . so sad . . . Time to find your next great movie!";
     watchlist.append(message);
   } else {
+    let newList = JSON.parse(list);
     newList.forEach((item) => {
       renderListItem(item);
     });
