99af3feed637460c2c03e8f4c815c7c772ac6420
kateaeryn <kate.ae.christensen@gmail.com>
2023-12-09 09:18:03 -0700

    vite config additions

---
+++ dist/assets/browse-c96afeac.js
@@ -0,0 +1,1 @@
+import{l as o,s as a}from"./utilities-40eb7b4b.js";o();a();
--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,11 @@ export default defineConfig({
       input: {
         home: resolve(__dirname, "src/index.html"),
         account: resolve(__dirname, "src/account.html"),
+        browse: resolve(__dirname, "src/browse.html"),
+        person: resolve(__dirname, "src/anything.html"),
+        movie: resolve(__dirname, "src/movieDetail.html"),
+        search: resolve(__dirname, "src/search.html"),
+        grid: resolve(__dirname, "src/movie-grid.html"),
       },
     },
   },
